a16e0727c4496555cc887d3f54c588676c34eba7
dmilz <dmilz@users.noreply.github.com>
2021-04-21 18:15:23 +0200

    Created wrapper class for time handling

---
+++ model/time.py
@@ -0,0 +1,23 @@
+
+
+
+class Time:
+  """Wrapper class for time"""
+  
+  def __init__(self):
+      pass
+    
+  def getDayOfWeek(self):
+      pass
+    
+  def isPublicHoliday(self):
+      pass
+    
+  def isSchoolHoliday(self):
+      pass
+    
+  def getDate(self):
+      pass
+    
+  def getTime(self):
+      pass
