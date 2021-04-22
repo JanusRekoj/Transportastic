59741646a204dbb8d303abddd0e01bcd90e37e41
Andreas <andreas.finkenzeller@tum.de>
2021-04-22 15:09:12 +0200

    Added simple Flask server structure

---
+++ controller.py
@@ -0,0 +1,44 @@
+# Imports
+from server import WebServer
+
+import time
+import threading
+
+
+class Controller:
+    def __init__(self) -> None:
+        # Init class attributes
+        # self.model = Model() # TODO: Create / Initialize model
+        self.server = WebServer(self, host='127.0.0.1', port=10000)
+        self._data = 0
+        self._data_lock = threading.Lock()
+        self._model_thread = threading.Thread(
+            target=self._run_model, daemon=True, args=(self._data_lock, ))
+
+        # Run model in dedicated worker thread
+        self._model_thread.start()
+
+        # Start server (blocking)
+        self.server.start_server()
+
+    def _run_model(self, lock: threading.Lock) -> None:
+        while True:
+            time.sleep(1)
+            lock.acquire()
+            self._data += 1
+            lock.release()
+
+    # TODO: Define interface methods that can be called from server to obtain data
+    # Here, we just need to call the appropriate methods in the model to actually
+    # get the requested data
+
+    def get_data(self) -> int:
+        self._data_lock.acquire()
+        #data = self._model.get_data()
+        data = self._data
+        self._data_lock.release()
+        return data
+
+
+# Main Script to launch app
+Controller()
---
+++ server.py
@@ -0,0 +1,51 @@
+# Use this import to avoid cyclic imports with type checking (requires Python >= 3.7)
+from __future__ import annotations
+
+# Imports
+import flask
+
+
+# Path to static HTML/JS/CSS files that are served for the WebUI
+# also check in APIController.py when handler gets bound to address
+static_html_folder = "webserver/web-ui/dist"
+
+
+class WebServer:
+    def __init__(self, controller: Controller, host: str = '0.0.0.0', port: int = 10000) -> None:
+        # Init class members
+        self._app = flask.Flask(__name__)
+        self._controller = controller
+        self._host = host
+        self._port = port
+
+        # Init routes
+        @self._app.route('/', methods=['GET'])
+        def index():
+            # Serve main website (index.html)
+            return flask.send_from_directory(static_html_folder, 'index.html')
+
+        @self._app.route('/data', methods=['GET'])
+        def data():
+            # Data interface
+            return "data = {}".format(self._controller.get_data())
+
+        @self._app.route('/favicon.ico', methods=['GET'])
+        def serve_favicon():
+            # Serve favicon
+            return flask.send_from_directory(static_html_folder, 'favicon.ico')
+
+        @self._app.route('/<path:p>/<filename>', methods=['GET'])
+        def serve_files(p, filename):
+            # Serve additional files
+            return flask.send_from_directory(static_html_folder+p, filename)
+
+    def start_server(self) -> None:
+        # Start server
+        self._app.run(host=self._host, port=self._port)
+        print('Start server on host ' + str(self._host) +
+              ' on port ' + str(self._port))
+
+    def stop_server(self) -> None:
+        # Stop server
+        print('Stop server on host ' + str(self._host) +
+              ' on port ' + str(self._port))
