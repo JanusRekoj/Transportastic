e53eae1850170b3d83d1b815c0d10d66a081eafd
Andreas <andreas.finkenzeller@tum.de>
2021-04-23 20:09:40 +0200

    Added team overview in About page

---
+++ webserver/pixida-web-ui/src/components/ProfileView.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="profile">
+    <div class="p-5">
+      <b-img
+        :src="getImgUrl(this.image)"
+        :width="200"
+        :height="200"
+        :alt="this.name"
+        class="m1"
+        rounded="circle"
+      ></b-img>
+      <p>{{ this.tmp }}</p>
+      <h1 class="pt-5">{{ this.name }}</h1>
+      <h4>{{ this.role }}</h4>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "NavBar",
+  components: {},
+  props: {
+    name: String,
+    role: String,
+    image: String,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    getImgUrl(pic) {
+      return require("../assets/profile_images/" + pic);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- webserver/pixida-web-ui/src/views/About.vue
+++ webserver/pixida-web-ui/src/views/About.vue
@@ -1,18 +1,65 @@
 <template>
   <div class="about">
-    <b-container class="text-left">
-      <p>This application was developed during the Hackathon 2021</p>
-    </b-container>
-    <img class="m-3" src="../assets/tum-logo.png" />
+    <h1 class="display-3 mx-5" >Team</h1>
+    <div class="row mx-3">
+      <div v-for="dev in developers" :key="dev.name" class="col-lg-4 col-md-6">
+        <profile-view :name="dev.name" :role="dev.role" :image="dev.image" />
+      </div>
+    </div>
+    <h1 class="display-3 mx-5" >Partners</h1>
+    <div>
+        <img class="m-3" height="50" center src="../assets/tum-logo.png" />
+        <img class="m-3" height="50" center src="../assets/lmu-logo.png" />
+        <img class="m-3" height="50" center src="../assets/pixida-logo.png" />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import ProfileView from "@/components/ProfileView.vue";
 
 export default {
   name: "About",
-  components: {},
+  components: {
+    ProfileView,
+  },
+  data() {
+    return {
+      developers: [
+        {
+          name: "Michael Maas",
+          role: "Team Leader",          
+          image: "michael.jpg",
+        },
+        {
+          name: "Johann Brenner",
+          role: "Frontend Dev",
+          image: "johann.jpg",
+        },
+        {
+          name: "Andreas Finkenzeller",
+          role: "Frontend Dev",
+          image: "andreas.png",
+        },
+        {
+          name: "Daniel Milz",
+          role: "Backend Dev",
+          image: "daniel.jpg",
+        },
+        {
+          name: "Alexander Schmaus",
+          role: "Backend Dev",
+          image: "alexander.jpg",
+        },
+        {
+          name: "Nika Nizharadze",
+          role: "Backend Dev",
+          image: "nika.jpg",
+        },
+      ],
+    };
+  },
 };
 </script>
 
