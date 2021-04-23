0693c2053b2e2cc62c0ec2f88f6ec4d94193d4c9
dmilz <dmilz@protonmail.com>
2021-04-23 18:42:06 +0200

    Implemented datastore and dataframe parser

--- model/datastore.py
+++ model/datastore.py
@@ -1,22 +1,23 @@
 import pandas as pd 
 
 class DataStore:
-    def __init__(self, path_root = '', ):
-
+    def __init__(self, debug=False):
+        path_root = ''
+        trips = ["trip_1","trip_2","trip_3","trip_4","trip_5","trip_6","trip_7","trip_8","trip_9","trip_10","trip_11","trip_12"]
+        df1 = []
+        df2 = []
+        df3 = []
+        for trip in trips:
+            df1.append(pd.read_csv(path_root+'data/' + trip + '/gps_data.csv',';'))
+            df2.append(pd.read_csv(path_root+'data/' + trip + '/passenger_count_nums',';'))
+            df3.append(pd.read_csv(path_root+'data/' + trip + '/wifi_data.csv',';'))
+        gpsdata = pd.concat(df1)
+        passenger = pd.concat(df2)
+        wifi = pd.concat(df3)
+        if debug:
+            print(gpsdata)
+            print(passenger)
+            print(wifi)
 
 if __name__ == "__main__":
-    path_root = '~/Projekte/ScienceHack/'
-    trips = ["trip_1","trip_2","trip_3","trip_4","trip_5","trip_6","trip_7","trip_8","trip_9","trip_10","trip_11","trip_12"]
-    df1 = []
-    df2 = []
-    df3 = []
-    for trip in trips:
-        df1.append(pd.read_csv(path_root+'data/' + trip + '/gps_data.csv',';'))
-        df2.append(pd.read_csv(path_root+'data/' + trip + '/passenger_count_nums',';'))
-        df3.append(pd.read_csv(path_root+'data/' + trip + '/wifi_data.csv',';'))
-    gpsdata = pd.concat(df1)
-    passenger = pd.concat(df2)
-    wifi = pd.concat(df3)
-    print(gpsdata)
-    print(passenger)
-    print(wifi)
+    ds = DataStore(debug=True)
