2d9375e8216d14d80b5f50c792e02acca9f90a75
Alex020796 <alexschmaus.96@gmail.com>
2021-04-24 13:30:07 +0200

    Alex: Paths in datastorage changed & Add dataprovider

---
+++ model/dataprovider.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class DataProvider:
+
+    def __init__(self, data):
+        self.data = data
+
+    def get_workload(self, str_bus_trip: str, start_time=np.datetime64('1900-01-01T00:00:00+00'), end_time=np.datetime64('2100-12-31T23:59:59')):
+        df_trip = self.data[self.data['line'] == str_bus_trip]
+        df_trip_timeslot = df_trip[(df_trip['epoch_ts']>start_time) & (df_trip['epoch_ts']<end_time)]
+        df_between_stations = df_trip_timeslot[~df_trip_timeslot['Number_of_Passengers'].isin([-1])]
+        return df_between_stations
+
+    def get_stations(self, str_bus_trip: str, start_time=np.datetime64('1900-01-01T00:00:00+00'), end_time=np.datetime64('2100-12-31T23:59:59')):
+        df_trip = self.data[self.data['line'] == str_bus_trip]
+        df_trip_timeslot = df_trip[(df_trip['epoch_ts']>start_time) & (df_trip['epoch_ts']<end_time)]
+        df_stations = df_trip_timeslot[df_trip_timeslot['Number_of_Passengers'].isin([-1])]
+        df_stations = df_stations[df_stations['speed_over_ground_knots'] < 1.0]
+        return df_stations
--- model/datastore.py
+++ model/datastore.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+import os
 
 dummy_response_dict = {
     'line123': {
@@ -42,24 +43,27 @@ class DataStore:
         df1l = []; df2l = []; df3l = []
         df2_time = []
         for trip in trips:
-            df1 = (pd.read_csv(path_root+'data/' + trip + '/gps_data.csv',','))
+            #df1 = (pd.read_csv(path_root+'data/' + trip + '/gps_data.csv',','))
+            df1 = (pd.read_csv(os.path.join(path_root, 'data', trip, 'gps_data.csv'), ','))
             df1 = df1.drop('date_time', 1)
             df1['epoch_ts'] = pd.to_datetime(df1['epoch_ts'], unit = 's')
             df1['line'] = trip
             df1['bus'] = 'bus_' + trip
             df1l.append(df1)
 
-            df2 = pd.read_csv(path_root+'data/' + trip + '/passenger_count_nums',',',comment='S') # Ignore Starting Time
+            #df2 = pd.read_csv(path_root+'data/' + trip + '/passenger_count_nums',',',comment='S') # Ignore Starting Time
+            df2 = (pd.read_csv(os.path.join(path_root, 'data', trip, 'passenger_count_nums'), ',',comment='S'))
             df2['epoch_ts'] = pd.to_datetime(df2['Epoch_Time'].astype(float).round().astype(int), unit = 's')
             df2 = df2.drop('Epoch_Time', 1)
-            with open(path_root + 'data/' + trip + '/passenger_count_nums', 'r') as f:
+            with open(os.path.join(path_root, 'data', trip, 'passenger_count_nums'), 'r') as f:
                 line = f.readlines()[1]
                 df2_time.append(line)
                 if debug:
                     print(line)
             df2l.append(df2)
 
-            df3 = (pd.read_csv(path_root+'data/' + trip + '/wifi_data.csv',';'))
+            #df3 = (pd.read_csv(path_root+'data/' + trip + '/wifi_data.csv',';'))
+            df3 = (pd.read_csv(os.path.join(path_root, 'data', trip, 'wifi_data.csv'), ';'))
             df3['epoch_ts'] = pd.to_datetime(df3['epoch_ts'], unit = 's')
             df3 = df3.drop('arr_ts', 1)
             df3l.append(df3)
