45c94a96c4ad6fd53af43a0670569e31f9e8ede8
Nika Nizharadze <nnizharadze@sdsu.edu>
2021-04-25 12:10:02 +0200

    Update wifi.py

--- model/wifi.py
+++ model/wifi.py
@@ -3,6 +3,12 @@ import pandas as pd
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats
+from scipy.stats import gaussian_kde
+import seaborn as sns
+import matplotlib 
+import scipy.stats as ss
 
 def mac_to_int(mac_str):
     # takes mac address as tring and converts to int
@@ -17,22 +23,23 @@ def get_passengers(df):
     flag = True
     current_station = set()
     next_station = set() 
-    count = 0
+    count = 1
     time = 0
     speed_flag = False
     start_index = 0
+    stop_count = 0
     for index, row in df.iterrows():
-        if flag:
+        if count%2 == 1:
             current_station.add(row['mac_address'])
         else:
             next_station.add(row['mac_address'])
-        if row['speed_over_ground_knots'] < 0.2:
+        if row['speed_over_ground_knots'] < 0.3:
             speed_flag = True
-        if row['speed_over_ground_knots'] > 3 and speed_flag:
+        if row['speed_over_ground_knots'] > 2 and speed_flag:
             count += 1
             flag = False
             speed_flag = False
-        if count == 2:
+        if count == 4:
             flag = True
             count = 0
             intersection = set.intersection(current_station, next_station)
@@ -53,27 +60,78 @@ def MSE(actual: np.ndarray, predicted: np.ndarray):
 
 def Evaluate(Y_true, predictions):
     print('MAE '+str(MAE(Y_true, predictions)))
-    print('MSE '+str(MSE(Y_true, predictions)))
+    # print('MSE '+str(MSE(Y_true, predictions)))
+
+def uniqueIndexes(l):
+    seen = set()
+    res = []
+    for i, n in enumerate(l):
+        if n not in seen:
+            res.append(i)
+            seen.add(n)
+    return res
+
 
 
 def calculate_accuracy(ground_truth, approximation):
     ground_truth = list(ground_truth)
     approximation = list(approximation)
     assert len(ground_truth) == len(approximation)
+    # uniquie_indices = uniqueIndexes(ground_truth)
+    # unique_list = []
+    # unique_list2 = []
+    # for i in range(len(uniquie_indices)):
+    #     unique_list.append(ground_truth[uniquie_indices[i]])
+    #     unique_list2.append(approximation[uniquie_indices[i]])
+    # # unique_list = ground_truth[uniquie_indices]
+    # print(unique_list)
+    # print(unique_list2)
     Evaluate(ground_truth, approximation)
+
+def plot(ground_truth, approximation):
+    sns.set(style="white", font_scale=1.8, font='Times New Roman')
+    # serif less font yu gothic
+
+
+    fig, ax = plt.subplots()
+    # ax.plot([0, 10, 20, 30], [0, 2, 1, 2])
+    # ax.tick_params(axis ='both', which ='major', 
+    #             labelsize = 16, pad = 12, 
+    #             colors ='r')
+    
+    # ax.tick_params(axis ='both', which ='minor',
+    #             labelsize = 8, colors ='b')
+    matplotlib.rcParams['font.size'] = 12
+    matplotlib.rcParams['figure.dpi'] = 100
+    # plt.figure()
+    # # sns.kdeplot(ground_truth,  label='Ground Truth', linewidth=2, linestyle="--", color='blue')
+    # # sns.displot(ground_truth, label='Ground Truth')
+    bins = np.linspace(0, 2, 40)
+    binwidth = 2
+    plt.hist(approximation, bins=[i for i in range(1,30)], histtype='bar',density=True, label='Ground Truth') ;
+    sns.kdeplot(approximation, label='Predictions', linewidth=2, linestyle="-", color='red') # dot
+
+    # # font size
+    plt.legend(prop={'size': 18},loc='upper right')
+    # # plt.title('Density Plot for Actual Number of Passengers vs Predicitons')
+    plt.xlabel('Number of Passengers')
+    plt.ylabel('Frequency');
+    plt.xticks(range(30))
+    plt.show()
+    # plt.savefig("dist_dam.png", bbox_inches='tight')
 
 
 if __name__ == "__main__":
-    ds = DataStore(debug=True)
-    data = ds.data
-    wifi = ds.wifi
+    # ds = DataStore(debug=True)
+    # data = ds.data
+    # wifi = ds.wifi
     # had too save first as a CSV, due to some merging issues
-    data.to_csv('data.csv', sep='\t')
-    wifi.to_csv('wifi.csv', sep='\t')
+    # data.to_csv('data.csv', sep='\t')
+    # wifi.to_csv('wifi.csv', sep='\t')
 
     data = pd.read_csv('data.csv', sep = '\t')
     wifi = pd.read_csv('wifi.csv', sep='\t')
-
+ 
     wifi['epoch_ts'] = wifi['epoch_ts'].str.split('.').str[0]
     df = pd.merge(wifi, data, on=['epoch_ts'])
 
@@ -87,5 +145,6 @@ if __name__ == "__main__":
     df = get_passengers(df)
     # Drop all the rows where number of passengers==-1,(unknown data samples)
     df = df[df['Number_of_Passengers'] > 0]
-
+    # df = df[df['Number_of_Passengers'] < 20]
     calculate_accuracy(df['Number_of_Passengers'], df['passengers'])
+    plot(df['Number_of_Passengers'], df['passengers'])
